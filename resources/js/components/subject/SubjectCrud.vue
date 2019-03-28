82cdee819474f2a35191b568a369c105dabcff30
Rogene Cris Violeta <rogenecris.violeta@flatworld.com.ph>
2019-03-28 11:47:06 +0800

    Created edit form

--- resources/js/components/subject/SubjectCrud.vue
+++ resources/js/components/subject/SubjectCrud.vue
@@ -1,36 +1,71 @@
 <template>
-  <table id="datatable-1" class="table table-datatable table-striped table-hover">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Status</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="subject in subjects.data" :key="subject.slug">
-        <td>{{ subject.name }}</td>
-        <td>
-          <div class="btn-group">
-            <select
-              name="status"
-              v-model="subject.status"
-              @change="updateStatus($event, subject.slug)"
-            >
-              <option :value="subject.status ? 1 : 0">{{ subject.status ? 'Active' : 'Inactive' }}</option>
-              <option :value="subject.status ? 0 : 1">{{ subject.status ? 'Inactive' : 'Active' }}</option>
-            </select>
-          </div>
-        </td>
-        <td>
-          <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
-            <button type="button" class="btn btn-danger" @click="deleteSubject(subject.slug)">Remove</button>
-            <button type="button" class="btn btn-warning">Edit</button>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div id="subject">
+    <table id="datatable-1" class="table table-datatable table-striped table-hover">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="subject in subjects.data" :key="subject.slug">
+          <td>{{ subject.name }}</td>
+          <td>
+            <div class="btn-group">
+              <select
+                name="status"
+                v-model="subject.status"
+                @change="updateStatus($event, subject.slug)"
+              >
+                <option :value="subject.status ? 1 : 0">{{ subject.status ? 'Active' : 'Inactive' }}</option>
+                <option :value="subject.status ? 0 : 1">{{ subject.status ? 'Inactive' : 'Active' }}</option>
+              </select>
+            </div>
+          </td>
+          <td>
+            <div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
+              <button
+                type="button"
+                class="btn btn-danger"
+                @click="deleteSubject(subject.slug)"
+              >Remove</button>
+              <button type="button" class="btn btn-warning" @click="editSubject">Edit</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div id="backdrop"></div>
+    <div id="main-modal">
+      <div class="card">
+        <div class="card-header">Edit</div>
+        <div class="card-body">
+          <form action>
+            <div class="form-group">
+              <label for="subject">Subject Name</label>
+              <input
+                type="text"
+                id="subject"
+                name="subject_name"
+                class="form-control"
+                placeholder="Subject Name"
+              >
+            </div>
+            <div class="form-group">
+              <label for="status">Status</label>
+              <select name="status" id="status" class="form-control">
+                <option value="1">Active</option>
+                <option value="0">Inactive</option>
+              </select>
+            </div>
+            <button class="btn btn-primary" type="submit">Update Subject</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -81,10 +116,40 @@ export default {
           swal("Subject was kept");
         }
       });
+    },
+    editSubject() {
+      const modal = document.getElementById("main-modal");
+      const backdrop = document.getElementById("backdrop");
     }
   }
 };
 </script>
 
 <style>
+#main-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 80%;
+  transform: translate(-50%, -50%);
+  z-index: 700;
+  display: none;
+}
+
+#backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 600;
+  background: rgba(0, 0, 0, 0.7);
+  display: none;
+}
+
+@media (min-width: 48em) {
+  #main-modal {
+    width: 30%;
+  }
+}
 </style>
